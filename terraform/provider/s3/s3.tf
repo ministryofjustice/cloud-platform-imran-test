272f21086400a0627ced1c2ebcd250a6ff02d7ad
Imran Awan <imran.awan@gec.io>
2020-05-12 12:31:54 +0100

    added new s3 bucket provider

--- terraform/provider/s3/s3.tf
+++ terraform/provider/s3/s3.tf
@@ -11,25 +11,6 @@ module "webops_vault_s3_bucket" {
 
   providers = {
     aws = aws.london
-
-
-  cors_rule =[
-    {
-      allowed_headers = ["*"]
-      allowed_methods = ["GET"]
-      allowed_origins = ["https://s3-website-test.hashicorp.com"]
-      expose_headers  = ["ETag"]
-      max_age_seconds = 3000
-    },
-    {
-      allowed_headers = ["*"]
-      allowed_methods = ["PUT"]
-      allowed_origins = ["https://s3-website-test.hashicorp.com"]
-      expose_headers  = [""]
-      max_age_seconds = 3000
-    },
-  ]
-
   }
 }
 
