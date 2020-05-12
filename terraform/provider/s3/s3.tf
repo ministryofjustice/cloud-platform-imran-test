cd767b093f5b34696f5d85f01a1cae656b94b3a1
Imran Awan <imran.awan@gec.io>
2020-05-12 12:28:11 +0100

    added new s3 bucket provider

--- terraform/provider/s3/s3.tf
+++ terraform/provider/s3/s3.tf
@@ -10,12 +10,8 @@ module "webops_vault_s3_bucket" {
   infrastructure-support = "platform@digtal.justice.gov.uk"
 
   providers = {
-    # Can be either "aws.london" or "aws.ireland"
     aws = aws.london
-    /*
-   * The following example can be used if you need to define CORS rules for your s3 bucket. 
-   *  Follow the guidance here "https://www.terraform.io/docs/providers/aws/r/s3_bucket.html#using-cors"
-   *  
+
 
   cors_rule =[
     {
