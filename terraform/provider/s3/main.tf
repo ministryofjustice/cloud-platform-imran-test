a31cef2b9424285b725ed04669d7f1cdb93a6b6d
Imran Awan <imran.awan@gec.io>
2020-05-13 11:12:01 +0100

    removed vault var

--- terraform/provider/s3/main.tf
+++ terraform/provider/s3/main.tf
@@ -6,26 +6,14 @@ terraform {
   }
 }
 
-provider "vault" {
- address = "${var.vault_addr}"
- token = "${var.vault_token}"
-}
-
-data "vault_aws_access_credentials" "creds" {
-  backend = "aws"
-  role    = "vault-role"
-}
-
 provider "aws" {
   region = "eu-west-1"
 }
 
 # To be use in case the resources need to be created in London
 provider "aws" {
-  access_key = "${data.vault_aws_access_credentials.creds.access_key}"
-  secret_key = "${data.vault_aws_access_credentials.creds.secret_key}"
   alias  = "london"
-  region = "${var.region}"
+  region = "eu-west-2"
 }
 
 # To be use in case the resources need to be created in Ireland
